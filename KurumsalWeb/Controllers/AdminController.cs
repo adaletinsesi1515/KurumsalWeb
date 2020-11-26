6b6d28e6b696b8427df94684aacc89a75f69c5ce
adaletinsesi15 <adaletinsesi15@gmail.com>
2020-11-26 23:09:55 +0300

    .
    .

--- KurumsalWeb/Controllers/AdminController.cs
+++ KurumsalWeb/Controllers/AdminController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using KurumsalWeb.Models.DataContext;
 
 namespace KurumsalWeb.Controllers
 {
     public class AdminController : Controller
     {
+        KurumsalDBContext db = new KurumsalDBContext();
+
         // GET: Admin
         public ActionResult Index()
         {
-            return View();
+            var sorgu = db.Kategori.ToList();
+            return View(sorgu);
         }
     }
 }
